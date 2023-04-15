4f4c3bb994c688d679374d9083e65c7eceb15286
Teo <testm1@Teos-MacBook-Pro.local>
2023-04-15 14:02:53 +0300

    hot fix

--- src/main/java/com/finalproject/airbnb/service/AbstractService.java
+++ src/main/java/com/finalproject/airbnb/service/AbstractService.java
@@ -1,28 +1,34 @@
 package com.finalproject.airbnb.service;
 
+import com.finalproject.airbnb.model.entities.Address;
+import com.finalproject.airbnb.model.entities.Property;
 import com.finalproject.airbnb.model.entities.User;
 import com.finalproject.airbnb.model.exceptions.NotFoundException;
+import com.finalproject.airbnb.model.repositories.CountryRepository;
 import com.finalproject.airbnb.model.repositories.PropertyRepository;
 import com.finalproject.airbnb.model.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AbstractService {
 
-    @Autowired
-    protected ModelMapper mapper;
+    protected final ModelMapper mapper;
 
-    @Autowired
-    protected UserRepository userRepository;
+    protected final UserRepository userRepository;
 
-    @Autowired
-    protected PropertyRepository propertyRepository;
+    protected final PropertyRepository propertyRepository;
+    protected final CountryRepository countryRepository;
 
     protected User getUserById(int id){
         return userRepository.findById(id).orElseThrow(() -> new NotFoundException("User not found"));
     }
 
-
+    protected Address fillAddress(Property property, int id){
+        int countryId = property.getAddress().getCountry().getId();
+        if(countryRepository.findCountriesById(countryId) == true)
+    }
 }
